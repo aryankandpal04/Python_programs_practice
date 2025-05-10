379b821b979b6acc4240179aa6d7dcee7d38e912
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-10 23:49:39 +0530

    update

---
+++ comments.py
@@ -0,0 +1,18 @@
+# this is single line comment
+
+'''
+this 
+is
+multiple line 
+comments
+'''
+
+"""
+    this 
+    is 
+    another
+    example of
+    multiple
+    lines 
+    comments
+"""
---
+++ variables_and_datatypes.py
@@ -0,0 +1,4 @@
+a = 5535
+print(a)
+b = "aryan"
+print(b)
