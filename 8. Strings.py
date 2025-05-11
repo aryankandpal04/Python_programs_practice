769c49cc2f8aa4fccfceba92ad4838dfc3321a3c
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-11 17:03:20 +0530

    update

---
+++ 8. Strings.py
@@ -0,0 +1,11 @@
+str = 'aryankandpal'
+print(str)
+print(str[5])
+
+str2 = 'halduchaur'
+print(str +" "+ str2)
+str3 = 'hello guys my name is \'aryan kandpal\''
+print(str3)
+str4 = '''this is another example of printing "strings" '''
+
+print(str4)
