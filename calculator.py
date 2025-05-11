548baa1c1ea099b309ca2934d8c96eb8edaccd38
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-11 10:47:35 +0530

    update

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 print("~*~*~*~*~*~*~ Simple Calculator ~*~*~*~*~*~*~")
-input(print("enter the num1 = "))
-input(print("enter the num2 = "))
+num1 = float(input("enter the num1 = "))
+num2 = float(input("enter the num2 = "))
+
+print("Addition = ",num1+num2)
+print("Substraction = ",num1-num2)
+print("Multiplication = ",num1*num2)
+print("Division = ",num1/num2)
+print("Floor Division = ",num1//num2)
+print("Exponents = ",num1**2)
+print("Modulos = ",num1%num2)
--- variables_and_datatypes.py
+++ variables_and_datatypes.py
@@ -11,6 +11,12 @@ c = True
 l = [1 , 'aryan' , 80.36 , True]
 n = None
 t = (1, "aryan" , 85.36 , False)
+s = {1,2,3,4}
+i = range(52)
+dist = {
+    'name' : ['aryan', 'aman' , 'ashish'],
+    'age'  : [21 , 22 , 23]
+}
 
 
 print("type of num1 = ", num1, type(num1))
@@ -20,3 +26,6 @@ print("type of c = ", c, type(c))
 print("type of l = ", l, type(l))
 print("type of n = ", n , type(n))
 print("type of t = ", t , type(t))
+print("type of s = ", s , type(s))
+print("type of i = ", i , type(i))
+print("type of dist = ", dist , type(dist))
