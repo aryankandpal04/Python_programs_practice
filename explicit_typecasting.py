aedd3ab8d9f3d7b0273672529a1f7109439c0cd7
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-11 13:03:18 +0530

    update

---
+++ explicit_typecasting.py
@@ -0,0 +1,17 @@
+print("\t\tExample No. 1\n")
+
+a = '20'
+b = '10'
+without_typecasting = a+b
+with_typecasting = int(a)+int(b)
+print("without typecasting a and b is ",without_typecasting, "\ntypeof without_casting is ", type(without_typecasting)) # without typecasting
+print("With typecasting a and b is ",with_typecasting, "\ntype of with typecasting is ", type(with_typecasting)) # with typecasting
+
+print("\n\t\tExample No. 2\n")
+
+c = '26'
+d = 2
+without_typecasting = c*d
+with_typecasting = int(a)*d
+print("without typecasting c and d is ",without_typecasting, "\ntypeof without_casting is ", type(without_typecasting)) # without typecasting
+print("With typecasting c and d is ",with_typecasting, "\ntype of with typecasting is ", type(with_typecasting)) # with typecasting
