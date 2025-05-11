c1958295a2c4ce19a58cea77521ef72938e9b580
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-11 14:01:01 +0530

    update

--- 7. implicit_typecasting.py
+++ 7. implicit_typecasting.py
@@ -1,5 +1,21 @@
+print("\t\tExample No. 1\n")
 a = 15.5
 b = 10
 
 add = a + b
-print("typecasting a and  is ",add, "\ntypeof a is", type(a), "\ntypeof b is", type(b), "\ntypeof with typecasting is ", type(add))
+print("typecasting a and b is ",add, "\ntypeof a is", type(a), "\ntypeof b is", type(b), "\ntypeof with typecasting is ", type(add))
+
+print("\n\t\tExample No. 2\n")
+a = True
+b = 10
+
+add = a+b
+print("typecasting a and b is ",add, "\ntypeof a is", type(a), "\ntypeof b is", type(b), "\ntypeof with typecasting is ", type(add))
+
+print("\n\t\tExample No. 3\n")
+a = 30
+b = 4 + 2j
+
+add = a+b
+print("typecasting a and b is ",add, "\ntypeof a is", type(a), "\ntypeof b is", type(b), "\ntypeof with typecasting is ", type(add))
+
