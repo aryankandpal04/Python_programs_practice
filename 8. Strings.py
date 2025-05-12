4851d28ee2d2870097f94a6944f8434dedec7cb8
aryankandpal04 <aryankandpal04@gmail.com>
2025-05-12 23:37:05 +0530

    update

--- 8. Strings.py
+++ 8. Strings.py
@@ -6,6 +6,15 @@ str2 = 'halduchaur'
 print(str +" "+ str2)
 str3 = 'hello guys my name is \'aryan kandpal\''
 print(str3)
-str4 = '''this is another example of printing "strings" '''
+str4 = 'this is another example of printing "strings" '
 
 print(str4)
+
+str5 = """ 
+this is an 'example'
+of printing 
+"multiple line "
+'''strings''' 
+"""
+
+print(str5)
---
+++ 9. string_slicing.py
@@ -0,0 +1,6 @@
+str1 = 'Hello my name is aryan kandpal'
+print(str1)
+str2 = 'I live in Halduchaur'
+print(str2)
+
+print(str1 + str2)
