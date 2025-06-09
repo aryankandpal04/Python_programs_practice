5a851a72fea0f3d0f9e9bcc67660e4d417f1bb92
aryankandpal04 <aryankandpal04@gmail.com>
2025-06-09 20:09:24 +0530

    update

--- 10. String_Method.py
+++ 10. String_Method.py
@@ -1,4 +1,11 @@
 str = 'AryanKandpal'
+str1 = 'Hello guys!!!!!!!!!!!!!!!!!'
+
+print(str)
+print(str1)
 print(len(str))
 print(str.upper())
 print(str.lower())
+print(str1.rstrip('!'))
+print(str1.replace('guys','aryan'))
+print(str.split())
